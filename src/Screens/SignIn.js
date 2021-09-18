36b8524dfc71dbe6d2c59a9084d5094493d6629c
KM-1002 <trinhtg1002@gmail.com>
2021-09-18 09:03:48 +0700

    18/9

--- src/Screens/SignIn.js
+++ src/Screens/SignIn.js
@@ -12,7 +12,6 @@ import {
     TouchableWithoutFeedback,
     Keyboard,
     Alert,
-
 } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import LinearGradient from 'react-native-linear-gradient';
@@ -29,7 +28,7 @@ const SignIn = ({ navigation }) => {
 
     const updateSecureTextEntry = () => {
         setcheck_textInputChange({
-            secureTextEntry: !check_textInputChange.secureTextEntry
+            secureTextEntry: !check_textInputChange.secureTextEntry 
 
         });
     }
